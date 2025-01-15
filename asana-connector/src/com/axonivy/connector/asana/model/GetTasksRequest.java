fe82507c3251a3d00e18ddebc44596b9920a4113
quanpham-axonivy <quan.phamvan@axonactive.com>
2025-01-15 14:00:31 +0700

    clean

--- asana-connector/src/com/axonivy/connector/asana/model/GetTasksRequest.java
+++ asana-connector/src/com/axonivy/connector/asana/model/GetTasksRequest.java
@@ -4,45 +4,70 @@ import java.util.List;
 
 public class GetTasksRequest {
 	
+	private String projectGid;
+
+	private String completedSince;
+
+	private String offset;
+
+	private Integer limit;
+
+	private List<String> optFields;
+
+	private Boolean optPretty;
+
 	public GetTasksRequest() {
 		super();
 	}
+
 	public String getProjectGid() {
 		return projectGid;
 	}
+
 	public void setProjectGid(String projectGid) {
 		this.projectGid = projectGid;
 	}
+
 	public String getCompletedSince() {
 		return completedSince;
 	}
+
 	public void setCompletedSince(String completedSince) {
 		this.completedSince = completedSince;
 	}
+
 	public String getOffset() {
 		return offset;
 	}
+
 	public void setOffset(String offset) {
 		this.offset = offset;
 	}
+
 	public Integer getLimit() {
 		return limit;
 	}
+
 	public void setLimit(Integer limit) {
 		this.limit = limit;
 	}
+
 	public List<String> getOptFields() {
 		return optFields;
 	}
+
 	public void setOptFields(List<String> optFields) {
 		this.optFields = optFields;
 	}
+
 	public Boolean getOptPretty() {
 		return optPretty;
 	}
+
 	public void setOptPretty(Boolean optPretty) {
 		this.optPretty = optPretty;
 	}
+
 	public GetTasksRequest(String projectGid, String completedSince, String offset, Integer limit,
 			List<String> optFields, Boolean optPretty) {
 		super();
@@ -53,16 +78,5 @@ public class GetTasksRequest {
 		this.optFields = optFields;
 		this.optPretty = optPretty;
 	}
-	private String projectGid;
-	
-	private String completedSince;
-	
-	private String offset;
-	
-	private Integer limit;
-	
-	private List<String> optFields;
-	
-	private Boolean optPretty;
 
 }
